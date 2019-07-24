e2bf1857d62e66faa4fc8f03bfcb312b0f0e1868
Hugo Burd <hugoburd@gmail.com>
2019-07-24 21:38:44 +0000

    fixed create-symliniks

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,4 +3,6 @@ ln -f .vimrc ~/.vimrc
 ln -f .tmux.conf ~/.tmux.conf
 mkdir -p ~/.vim
 ln -f -s ~/config/autoload ~/.vim/autoload
+
+mkdir ~/.config/nvim
 ln -f -s ~/config/nvim/init.vim ~/.config/nvim/init.vim
