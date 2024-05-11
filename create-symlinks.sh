3504a0472af2400dfd837dfe65efd2f252e1f5be
Hugo Burd <hugo@hugo-mcss.localdomain>
2024-05-11 15:39:20 -0500

    disable terminator and bashrc, force ln target not to be directory

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p ~/.local/share/nvim/site
-ln -f -s ~/config/autoload ~/.local/share/nvim/site/autoload
+ln -sT ~/config/autoload ~/.local/share/nvim/site/autoload
 
-ln -s ~/config/nvim ~/.config/nvim
+ln -sT ~/config/nvim ~/.config/nvim
 
-mkdir -p ~/.config/terminator
-ln -f -s ~/config/terminator_config ~/.config/terminator/config
-
-rm -f ~/.bashrc
-ln -f -s ~/config/.bashrc ~/.bashrc
+#mkdir -p ~/.config/terminator
+#ln -f -s ~/config/terminator_config ~/.config/terminator/config
+#
+#rm -f ~/.bashrc
+#ln -f -s ~/config/.bashrc ~/.bashrc
